4e6dbcc7f292ffe71019dc350156856c1ec06692
hoahuy <hoai.huynh@publicis-modem.fr>
2017-03-23 17:58:40 +0100

    js close popin on escape key
    Change-Id: Ia00fd32b310d9d9f4ab75d701cf3191a9fe73ae6

---
+++ html/js/toggler-plugin/popin.js
@@ -0,0 +1,55 @@
+/**
+ * init
+ */
+var popinToggler = (function () {
+
+    /*
+     *  use escape key to close popin
+     */
+
+    var $popin = $('[data-toggler-type="popin"]');
+    var popinTab = [];
+
+    var openedPopin = function (elem) {
+        var $currentToggler = $(elem.target);
+
+        if($currentToggler.data('toggler-type') == 'popin') {
+            //when several popins opened, add class on below ones
+            if (popinTab.length > 0) {
+                popinTab[popinTab.length - 1].addClass('under');
+            }
+            //store popin
+            popinTab.push($currentToggler);
+        }
+    };
+
+    var closedPopin = function (elem) {
+        if($(elem.target).data('toggler-type') == 'popin') {
+            //remove tab last push
+            popinTab.splice(-1,1);
+
+            //remove class on previous popin
+            if(popinTab.length > 0) {
+                popinTab[popinTab.length - 1].removeClass('under');
+            }
+        }
+    };
+
+    var escapeKey = function (e) {
+        if (popinTab.length > 0 && e.keyCode == 27) {
+            //close last open popin
+            popinTab[popinTab.length - 1].find('.popin-close').trigger('click');
+        }
+    };
+
+    var init = function () {
+        $(document).on('open.content', $popin, openedPopin).on('close.content', $popin, closedPopin).on('keyup', escapeKey);
+    };
+
+    return {
+        init: init
+    }
+
+})();
+
+export default popinToggler;
