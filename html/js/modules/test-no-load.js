3c8c9cbdf72241563a8edf01bffc45bb99eba09c
antsan2 <antoine.sanchez@nurun.com>
2017-04-03 16:41:54 +0200

    add js documentation for #11 JS scripts & module documentation
    * also migrate module to nu6tils (updated to 1.2.1)

--- html/js/modules/test-no-load.js
+++ html/js/modules/test-no-load.js
@@ -3,7 +3,7 @@
  * example with module.parse()
  */
 
-let module = require("../core/module");
+import {webModule} from 'nu6tils';
 
 let moduleTest = function (elem, data) {
     let ready = function () {
@@ -11,7 +11,7 @@ let moduleTest = function (elem, data) {
         // console.info(`le module test-no-load a été init au DOMReady via l'élément`, elem);
         $(elem).on('click', function () {
             elem.insertAdjacentHTML('afterEnd', `<h2 class="js-module" data-module="test-no-load" data-module-test-no-load--id="${data.id}">Test no-load ${data.id}</h2>`);
-            module.parse($('.js-module'));
+            webModule.parse($('.js-module'));
         });
 
     };
